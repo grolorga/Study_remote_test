75fbb42af64dc45f76d8fb161e8e85e0687d345e
grolorga <106234511+grolorga@users.noreply.github.com>
2025-10-18 15:06:21 +0500

    Update MainActivity.kt

--- app/src/main/java/com/example/study/MainActivity.kt
+++ app/src/main/java/com/example/study/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var number2EditText: EditText
     private lateinit var resultTextView: TextView
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
